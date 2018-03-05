ad2075d57cc7883785967e23e75aeaa86f7c62b6
sangwoo <mesyu17539@gmail.com>
2018-03-05 13:06:13 +0900

    mapper

---
+++ Bitcamp/src/main/java/com/bitcamp/app/mapperImpl/MemberMapperImpl.java
@@ -0,0 +1,33 @@
+package com.bitcamp.app.mapperImpl;
+
+import com.bitcamp.app.command.Command;
+import com.bitcamp.app.command.ResultMap;
+import com.bitcamp.app.mapper.MemberMapper;
+
+public class MemberMapperImpl implements MemberMapper{
+
+	@Override
+	public ResultMap insertMember(Command cmd) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ResultMap selectById(Command cmd) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ResultMap login(Command cmd) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ResultMap delete(Command cmd) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
