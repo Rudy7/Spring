02cd3622aaf351f4073f8714ef35b6e5f1f1b052
sangwoo <mesyu17539@gmail.com>
2018-03-05 10:45:24 +0900

    MyBatis

--- Bitcamp/src/main/java/com/bitcamp/app/mapper/MemberMapper.java
+++ Bitcamp/src/main/java/com/bitcamp/app/mapper/MemberMapper.java
@@ -1,14 +1,20 @@
-package com.bitcamp.app.service;
+package com.bitcamp.app.mapper;
 
 import com.bitcamp.app.command.Command;
 import com.bitcamp.app.command.ResultMap;
 import com.bitcamp.app.domain.MemberBean;
 
-public interface MemberService {
+public interface MemberMapper {
 	/*public abstract MemberBean findById();*///디폴트이며 생략가능한 이유는 다른건 못오기 때문이다
 	public ResultMap join(Command cmd);
-	public ResultMap login(Command cmd);
+//	public ResultMap login(Command cmd);
 	public ResultMap chage(Command cmd);
 	public ResultMap leave(Command cmd);
 	public ResultMap memjoin(Command cmd);
+	
+	public ResultMap insert(Command cmd);
+	public ResultMap selectById(Command cmd);
+	public ResultMap login(Command cmd);
+	public ResultMap delete(Command cmd);
+	
 }
